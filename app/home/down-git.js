6ec3cb677fd42eb094ed367f454fea5984f1af62
Kavin <113820010+wert18@users.noreply.github.com>
2025-03-29 15:51:03 +0530

    Add files via upload

--- app/home/down-git.js
+++ app/home/down-git.js
@@ -15,7 +15,7 @@ downGitModule.factory('downGitService', [
 
             info.author = splitPath[1];
             info.repository = splitPath[2];
-            info.branch = splitPath[4];
+            info.branch = splitPath[4] || "main"; // Default to main branch if not specified
 
             info.rootName = splitPath[splitPath.length-1];
             if(!!splitPath[4]){
@@ -23,9 +23,13 @@ downGitModule.factory('downGitService', [
                     repoPath.indexOf(splitPath[4])+splitPath[4].length+1
                 );
             }
-            info.urlPrefix = "https://api.github.com/repos/"+
+            
+            // Use raw GitHub content URL instead of API
+            info.urlPrefix = "https://raw.githubusercontent.com/"+
+                info.author+"/"+info.repository+"/"+info.branch+"/";
+            info.apiUrlPrefix = "https://api.github.com/repos/"+
                 info.author+"/"+info.repository+"/contents/";
-            info.urlPostfix = "?ref="+info.branch;
+            info.apiUrlPostfix = "?ref="+info.branch;
 
             if(!parameters.fileName || parameters.fileName==""){
                 info.downloadFileName = info.rootName;
@@ -35,11 +39,9 @@ downGitModule.factory('downGitService', [
 
             if(parameters.rootDirectory=="false"){
                 info.rootDirectoryName = "";
-
             } else if(!parameters.rootDirectory || parameters.rootDirectory=="" ||
                 parameters.rootDirectory=="true"){
                 info.rootDirectoryName = info.rootName+"/";
-
             } else{
                 info.rootDirectoryName = parameters.rootDirectory+"/";
             }
@@ -59,11 +61,10 @@ downGitModule.factory('downGitService', [
         }
 
         var mapFileAndDirectory = function(dirPaths, files, requestedPromises, progress){
-            $http.get(repoInfo.urlPrefix+dirPaths.pop()+repoInfo.urlPostfix).then(function(response) {
+            $http.get(repoInfo.apiUrlPrefix+dirPaths.pop()+repoInfo.apiUrlPostfix).then(function(response) {
                 for(var i=response.data.length-1; i>=0; i--){
                     if(response.data[i].type=="dir"){
                         dirPaths.push(response.data[i].path);
-
                     } else{
                         if(response.data[i].download_url){
                             getFile(response.data[i].path,
@@ -81,6 +82,10 @@ downGitModule.factory('downGitService', [
                 } else{
                     mapFileAndDirectory(dirPaths, files, requestedPromises, progress);
                 }
+            }).catch(function(error) {
+                console.error("Error fetching directory:", error);
+                progress.isProcessing.val = false;
+                toastr.error("Error accessing GitHub repository. Please check if the repository is public and accessible.");
             });
         }
 
@@ -98,6 +103,10 @@ downGitModule.factory('downGitService', [
                 zip.generateAsync({type:"blob"}).then(function(content) {
                     saveAs(content, repoInfo.downloadFileName+".zip");
                 });
+            }).catch(function(error) {
+                console.error("Error creating zip:", error);
+                progress.isProcessing.val = false;
+                toastr.error("Error creating download file. Please try again.");
             });
         }
 
@@ -105,8 +114,14 @@ downGitModule.factory('downGitService', [
             var promise = $http.get(url, {responseType: "arraybuffer"}).then(function (file) {
                 files.push({path:path, data:file.data});
                 progress.downloadedFiles.val = files.length;
-            }, function(error) {
-                console.log(error);
+            }).catch(function(error) {
+                console.error("Error downloading file:", error);
+                // Try alternative URL if API fails
+                var altUrl = repoInfo.urlPrefix + path;
+                return $http.get(altUrl, {responseType: "arraybuffer"}).then(function (file) {
+                    files.push({path:path, data:file.data});
+                    progress.downloadedFiles.val = files.length;
+                });
             });
 
             requestedPromises.push(promise);
@@ -127,10 +142,23 @@ downGitModule.factory('downGitService', [
                 zip.generateAsync({type:"blob"}).then(function(content) {
                     saveAs(content, repoInfo.downloadFileName+".zip");
                 });
-            }, function(error) {
-                console.log(error);
-                progress.isProcessing.val=false;
-                toastr.warning("Error! Server failure or wrong URL.", {iconClass: 'toast-down'});
+            }).catch(function(error) {
+                console.error("Error downloading file:", error);
+                // Try alternative URL if API fails
+                var altUrl = repoInfo.urlPrefix + repoInfo.resPath;
+                $http.get(altUrl, {responseType: "arraybuffer"}).then(function (file) {
+                    progress.downloadedFiles.val = 1;
+                    zip.file(repoInfo.rootName, file.data);
+
+                    progress.isProcessing.val=false;
+                    zip.generateAsync({type:"blob"}).then(function(content) {
+                        saveAs(content, repoInfo.downloadFileName+".zip");
+                    });
+                }).catch(function(error) {
+                    console.error("Error downloading file:", error);
+                    progress.isProcessing.val=false;
+                    toastr.error("Error downloading file. Please check if the repository is public and accessible.");
+                });
             });
         }
 
@@ -140,24 +168,23 @@ downGitModule.factory('downGitService', [
 
                 if(!repoInfo.resPath || repoInfo.resPath==""){
                     if(!repoInfo.branch || repoInfo.branch==""){
-                        repoInfo.branch="master";
+                        repoInfo.branch="main";
                     }
 
                     var downloadUrl = "https://github.com/"+repoInfo.author+"/"+
                         repoInfo.repository+"/archive/"+repoInfo.branch+".zip";
 
                     window.location = downloadUrl;
-
-                }else{
-                    $http.get(repoInfo.urlPrefix+repoInfo.resPath+repoInfo.urlPostfix).then(function(response) {
+                } else {
+                    $http.get(repoInfo.apiUrlPrefix+repoInfo.resPath+repoInfo.apiUrlPostfix).then(function(response) {
                         if(response.data instanceof Array){
                             downloadDir(progress);
-                        }else{
+                        } else {
                             downloadFile(response.data.download_url, progress, toastr);
                         }
-
-                    }, function(error) {
-                        console.log("probable big file.");
+                    }).catch(function(error) {
+                        console.error("Error accessing repository:", error);
+                        // Try direct download if API fails
                         downloadFile("https://raw.githubusercontent.com/"+repoInfo.author+"/"+
                                 repoInfo.repository+"/"+repoInfo.branch+"/"+repoInfo.resPath,
                                 progress, toastr);
